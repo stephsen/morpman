23c489e28b00eb234eab469704d35bebb398b01f
stephsen <stephsen59@gmail.com>
2016-12-20 17:59:10 +0100

    third trial

--- morp.js
+++ morp.js
@@ -24,15 +24,21 @@ var kill = (bill = ["", "", "", "", "", "", "", "", ""], counter = 0, victory = 
     return "<form action='/' method='post'><table><tr><td><input value='" + bill[0] + "' name='bill[0]'/></td><td><input value='" + bill[1] + "' name='bill[1]'/></td><td><input value='" + bill[2] + "' name='bill[2]'/></td></tr><tr><td><input value='" + bill[3] + "' name='bill[3]'/></td><td><input value='" + bill[4] + "' name='bill[4]'/></td><td><input value='" + bill[5] + "' name='bill[5]'/></td></tr><tr><td><input value='" + bill[6] + "' name='bill[6]'/></td><td><input value='" + bill[7] + "' name='bill[7]'/></td><td><input value='" + bill[8] + "' name='bill[8]'/></td></tr></table><input name='counter' type='hidden' value='" + counter + "'><input name='previous' type='hidden' value='" + previous + "'><input name='button' type='submit'><input name='victory' type='text' value='" + victory + "' style='width: 280px;'><button name='newgame'><a href='/'>New game</a></button></form>";
 };
 
-var test = (myArray, index1, index2, index3, signeX, signeO) => {
-    if ([myArray[index1], myArray[index2], myArray[index3]].every(elem => elem === signeX)) {
-        return "Le joueur X a gagné";
-    } else if ([myArray[index1], myArray[index2], myArray[index3]].every(elem => elem === signeO)) {
-        return "Le joueur O a gagné";
-    } else {
-      return "";
+var test = (myArray, tab, signeX, signeO) => {
+    var message = "";
+    for (var i = 0; i <= tab.length - 3; i = i + 3) {
+        console.log(tab[i], tab[i + 1], tab[i + 2]);
+        if (myArray[tab[i]] == signeX && myArray[tab[i + 1]] == signeX && myArray[tab[i + 2]] == signeX) {
+            message = "Le joueur X a gagné";
+        } else if (myArray[tab[i]] == signeO && myArray[tab[i + 1]] == signeO && myArray[tab[i + 2]] == signeO) {
+            message = "Le joueur O a gagné";
+        } else {
+            message = "";
+        }
     }
+    return message;
 };
+
 app.get('/', (req, res) => {
     res.send(kill());
 });
@@ -40,49 +46,25 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
     var myArray = req.body.bill.map(b => b !== "" ? b.toUpperCase() : b),
         counter = Number(req.body.counter),
-        i = 0;
-
+        i = 0,
+        tab = {0: [0, 1, 2, 0, 4, 6, 0, 4, 8],1: [0, 1, 2, 1, 4, 7],2: [0, 1, 2, 2, 5, 8, 2, 4, 6],
+            3: [3, 4, 5, 0, 3, 6],4: [4, 3, 5, 4, 1, 7, 4, 2, 6, 4, 0, 8],5: [5, 4, 3, 5, 2, 8],6: [6, 3, 0, 6, 7, 8, 6, 4, 2],
+            7: [7, 6, 8, 7, 4, 1],8: [8, 7, 6, 8, 5, 2, 8, 4, 0]};
     var a = myArray.reduce((prev, curr) => (prev[curr] = ++prev[curr] || 1, prev), {});
     if ((isNaN(a.X) ? 0 : a.X) - (isNaN(a.O) ? 0 : a.O) >= -1 && (isNaN(a.X) ? 0 : a.X) - (isNaN(a.O) ? 0 : a.O) <= 1) {
         let victory = "";
         let previous = req.body.previous.split(",");
         for (let j in myArray) {
             if (myArray[j] != previous[j]) {
+                // console.log(tab[j]);
+                victory = test(myArray, tab[j], "X", "O");
                 i = Number(j);
                 previous[j] = myArray[j];
                 counter++;
             }
         }
-        switch (i) {
-            case 0:
-                victory = test(myArray, i, i + 1, i + 2, "X", "O") || test(myArray, i, i + 3, i + 6, "X", "O") || test(myArray, i, i + 4, i + 8, "X", "O");
-                break;
-            case 1:
-                victory = test(myArray, i, i - 1, i + 1, "X", "O") || test(myArray, i, i + 3, i + 6, "X", "O");
-                break;
-            case 2:
-                victory = test(myArray, i, i - 1, i - 2, "X", "O") || test(myArray, i, i + 3, i + 6, "X", "O") || test(myArray, i, i + 2, i + 4, "X", "O");
-                break;
-            case 3:
-                victory = test(myArray, i, i + 1, i + 2, "X", "O") || test(myArray, i, i - 3, i + 3, "X", "O");
-                break;
-            case 4:
-                victory = test(myArray, i, i - 1, i + 1, "X", "O") || test(myArray, i, i - 3, i + 3, "X", "O") || test(myArray, i, i - 4, i + 4, "X", "O") || test(myArray, i, i - 2, i + 2, "X", "O");
-                break;
-            case 5:
-                victory = test(myArray, i, i - 1, i - 2, "X", "O") || test(myArray, i, i - 3, i + 3, "X", "O");
-                break;
-            case 6:
-                victory = test(myArray, i, i + 1, i + 2, "X", "O") || test(myArray, i, i - 3, i - 6, "X", "O") || test(myArray, i, i - 2, i - 4, "X", "O");
-                break;
-            case 7:
-                victory = test(myArray, i, i - 1, i + 1, "X", "O") || test(myArray, i, i - 3, i - 6, "X", "O");
-                break;
-            case 8:
-                victory = test(myArray, i, i - 1, i - 2, "X", "O") || test(myArray, i, i - 3, i - 6, "X", "O") || test(myArray, i, i - 4, i - 8, "X", "O");
-                break;
-        }
-        console.log(victory, counter);
+
+        // console.log(victory, counter);
         if (victory === "") {
             if (counter == 9) {
                 res.send(kill(myArray, counter, "Match null", previous));
